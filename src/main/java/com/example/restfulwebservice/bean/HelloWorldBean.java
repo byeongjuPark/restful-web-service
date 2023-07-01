2dd7fefc2df150ce5c62b4cb8fba925bd8df68c9
byeongjuPark <bottlepark28@gmail.com>
2023-07-01 21:52:26 +0900

    yml 설정

---
+++ src/main/java/com/example/restfulwebservice/bean/HelloWorldBean.java
@@ -0,0 +1,16 @@
+package com.example.restfulwebservice.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// lombok
+// @Data HelloWorldBean 클래스의 getter, setter, toString 제공
+// @AllArgsConstructor 생성자 제공
+// @NoArgsConstructor 기본 생성자 제공
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldBean {
+    private String message;
+}
