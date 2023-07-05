071d8695ec6633e76b8c1e497da39bb641ad4522
byeongjuPark <bottlepark28@gmail.com>
2023-07-05 23:05:24 +0900

    다국어 처리

--- src/main/java/com/example/restfulwebservice/RestfulWebServiceApplication.java
+++ src/main/java/com/example/restfulwebservice/RestfulWebServiceApplication.java
@@ -1,13 +1,28 @@
 package com.example.restfulwebservice;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class RestfulWebServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServiceApplication.class, args);
+	}
+
+	
+	// @SpringBootApplication 을 가지고 있는 클래스에 Bean을 등록하게 되면
+	// Spring Boot가 초기화 될 때 해당하는 정보가 같이 메모리에 올라가게 된다.
+	@Bean
+	public LocaleResolver localeResolver(){
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.KOREA);
+		return localeResolver;
 	}
 
 }
--- src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
@@ -1,12 +1,22 @@
 package com.example.restfulwebservice.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
+
     // GET
     // /hello-world (endpoint)
     // @RequestMapping(method = RequestMethod.GET, path="/hello-wolrd")
@@ -19,6 +29,10 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldBean(){
         return new HelloWorldBean("bean");
     }
-    
-    
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required = false) Locale locale){
+        return messageSource.getMessage("greeting.message", null, locale);
+
+    }
 }
